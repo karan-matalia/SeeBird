fccfb72e5caf1d8c8b0e8e0b42a8ce626a488699
Karan Matalia <e3nextcustomerservice@gmail.com>
2020-07-18 18:51:31 +0530

    add license

--- src/driver.py
+++ src/driver.py
@@ -1,5 +1,25 @@
 """
-a program to perform all ebird actions.
+MIT License
+
+Copyright (c) 2020 Karan Matalia
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 from src.controllers import get_life_list, get_location_based_lifers, get_region_list, compare_year_list, \
     summarise_checklists, compare_hotspots, get_possible_lifers_in_a_hotspot, get_possible_lifers_in_a_trip, \
